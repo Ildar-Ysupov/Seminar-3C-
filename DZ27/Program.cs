14da389d4879f074264c2666118ba6a932baddbe
Ildar-Ysupov <aismen56@mail.ru>
2022-08-19 14:02:10 +0500

    DZ27

---
+++ DZ27/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 27:
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for (int n = number ; n > 0; n = n / 10) 
+{
+    sum = sum + n % 10;
+}
+Console.WriteLine($"Сумма цифр числа {number} = {sum}");
