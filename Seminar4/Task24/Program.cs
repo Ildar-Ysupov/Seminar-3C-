8fdd7951040fec347dfd357ae132d3f905208493
Ildar-Ysupov <aismen56@mail.ru>
2022-08-19 11:23:13 +0500

    Seminar4

---
+++ Seminar4/Task24/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 24:
+//  Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36  
+
+// 1: способ
+
+// Console.Write("Введите число A: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+
+// // sum [1;A]
+// // for (начало; условие; увеличение счётчика)
+// int sum = 0;
+
+// for (int i = 1; i <= A; i++)
+// {
+//     sum += i;
+// }
+// Console.WriteLine($"Сумма элементов от 1 до {A} = {sum}");
+
+// 2: Способ через метод и цикл while.
+
+Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+// метод - функция созданная собственноручно
+// тип_возвращаемого_значения (int; string; double) NameOfMethod (params)
+// int GetSumNumbers(int number) - получить сумму чисел где: int-возвращаем(return)
+// переменную числового типа; GetSumNumbers-имя метода; (int number)-параметор метода.
+int GetSumNumbers(int number)
+{
+    int sum = 0;
+    int start = 1;
+    while(start <= number)
+    {
+        sum += start;
+        start ++;
+    }
+    return sum;
+}
+Console.WriteLine($"Сумма элементов от 1 до {A} = {GetSumNumbers(A)}");
+// return аналог Console.WriteLine
+// return вщзвращает результат к месту вызова метода
---
+++ Seminar4/Task26_28_30/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 26:
+// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int GetCountNumbers(int digit)
+// {
+//     int count = 0; // чисел 0
+//     while (digit > 0)
+//     {
+//         count++;
+//         digit /= 10;
+//     }
+//     return count;
+// }
+
+// Console.WriteLine(GetCountNumbers(number));
+
+// Задача 28:
+//  Напишите программу, которая принимает на вход число N и выдаёт
+//   произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int GetMultiply(int num)
+// {
+//     // [1;N]
+//     int result = 1; //умножение на 0 даст вам 0
+//     // 5! = 1*2*3*4*5
+//     for (int i = 2; i <= num; i++) // 1! = 1,
+//     {
+//         result *= i;
+//     }
+//     return result;
+// }
+// Console.WriteLine($"Произведение чисел от 1 до {number} = {GetMultiply(number)}");
+
+// Задача 30:
+//  Напишите программу, которая выводит массив из 8 элементов,
+//  заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] GetBinaryArray(int size)
+{
+    int[] result = new int[size]; // size = 5; result = [0,0,0,0,0]
+    for (int i = 0; i < result.Length; i++) // size = result.Length
+    {
+        //[min; max+1);
+        result[i] = new Random().Next(2); //Если указано одно число, то вы идете от 0 до этого числа
+    }
+    return result;
+}
+
+int [] testArray = GetBinaryArray(8);
+Console.WriteLine($"Результат: [ {String.Join("; ", testArray)} ]");
