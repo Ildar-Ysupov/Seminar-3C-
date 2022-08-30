1a753b3d2303851dd73272993dfe3e2e27dc0688
Ildar-Ysupov <aismen56@mail.ru>
2022-08-30 16:12:14 +0500

    DZ50

---
+++ DZ50/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 50.
+//  Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Console.WriteLine("Введите позицию элемента в строке: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите позицию элемента в столбце: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int rows = 4;
+int columns = 4;
+
+int[,] GetArray(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(11);
+        }
+    }
+    return matrix;
+}
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] resultMatrix = GetArray(rows, columns);
+PrintArray(resultMatrix);
+Console.WriteLine();
+
+if (a <= rows && b <= columns)
+{
+   
+    Console.WriteLine(resultMatrix[a - 1 , b - 1]);
+}
+else
+    Console.WriteLine("В данном массиве элемента с такими позициями не существует!");
